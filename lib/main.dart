6b5e54248f1a863540bed4963ce62e3e92e29ef0
SherbekFayzullayev <sherbekcode.2328@gmail.com>
2023-08-19 11:24:56 +0500

    container

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,147 @@
+// import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+void main () {
+  runApp(MaterialApp(
+    home: Container(
+    color: Colors.blue, 
+    child: Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          width:95,
+          height: 100,
+          decoration:const BoxDecoration(
+            color: Colors.red,
+            borderRadius:BorderRadius.only(
+              bottomLeft: Radius.circular(50),
+              bottomRight:Radius.circular(50),
+              topLeft: Radius.circular(50),
+              topRight: Radius.circular(50),
+            ) 
+          ),
+          child:  const Text("+",style: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.yellow,
+            fontSize: 40
+          )),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+        Container(
+          margin: EdgeInsets.all(20),
+          width: 300,
+          height: 80,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 3,
+              color: Colors.red,
+            ),
+            color: Colors.green,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(0),
+              bottomRight:Radius.circular(0),
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            )
+          ),
+          
+          child: Text('Flutter',style: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.white,
+            fontSize: 41,
+          ),),
+          alignment: Alignment.center,
+        ),
+       Container(
+          margin: EdgeInsets.all(20),
+          width: 300,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 2,
+              color: Colors.black,
+            ),
+            gradient:LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+              Colors.yellow,
+              Colors.white,
+            ]),
+            color: Colors.grey,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(0),
+              bottomRight: Radius.circular(20),
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(0),
+            ),
+          ),
+          child: Text('Flutter',style: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.green,
+            fontSize: 41, 
+            
+          )),
+          alignment: Alignment.center,
+          ),
+          Container(
+            width: 300,
+           height: 100,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft,
+                colors: [
+                Colors.blue, 
+                Colors.white,
+              ]),
+              color: Color.fromARGB(255, 213, 30, 233),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              )
+            ),
+           alignment: Alignment.center, 
+          margin: EdgeInsets.all(30),
+          child:const Text('Flutter',style: TextStyle(
+            decoration: TextDecoration.none,
+            color: Color.fromARGB(255, 76, 109, 175),
+            fontSize: 41, 
+          )),
+          ),
+          Container(
+            width: 300,
+           height: 100,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft,
+                colors: [
+                Color.fromARGB(255, 163, 33, 243), 
+                Color.fromARGB(255, 10, 209, 192),
+              ]),
+              color: Color.fromARGB(255, 213, 30, 233),
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(60),
+                topLeft: Radius.circular(60),
+              )
+            ),
+           alignment: Alignment.center, 
+          margin: EdgeInsets.all(30),
+          child:const Text('CODEACADEMY',style: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.black,
+            fontSize: 30,
+          )
+          ),
+          ),
+
+      ],
+    ) ,
+    ),
+  ));
 }
+  
+
